16b579923dd1b6e42a2e067a127e8126a8372b39
Xuan you Lim <xlim@e1z2r1p6.42.us.org>
2018-08-15 09:58:46 -0700

    Fix strjoin

--- additional-ft/ft_strjoin.c
+++ additional-ft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: xlim <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 21:34:21 by xlim              #+#    #+#             */
-/*   Updated: 2018/07/15 22:32:15 by xlim             ###   ########.fr       */
+/*   Updated: 2018/08/15 09:58:27 by xlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ char		*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*out;
 
-	if (!s1 || !s2)
-		return (NULL);
 	out = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (!out)
 		return (NULL);
